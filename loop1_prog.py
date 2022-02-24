08bc33144f184a7ce4eea4063a44a1f6d08f2ad3
admin <sainikamaljitsingh@gmail.com>
2022-02-24 11:56:15 +0530

    Python Programs 20BCS-719_A

--- loop1_prog.py
+++ loop1_prog.py
@@ -6,3 +6,27 @@ for number in num_list:
     for letter in alpha_list:
         print(letter, end = "")
     print()
+"""
+
+"""sharks = ['hammerhead', 'great white', 'dogfish', 'frilled', 'bullhead', 'requiem']
+
+for shark in sharks:
+    for letter in shark:
+        print(letter, end = "")
+    print()
+
+
+
+sammy_shark = {'name': 'Sammy', 'animal': 'shark', 'color': 'blue', 'location': 'ocean', 'Quantity': '23'}
+
+for key in sammy_shark:
+   print(key + ': ' + sammy_shark[key])
+
+"""
+
+list_of_lists = [['hammerhead', 'great white', 'dogfish'],[0, 1, 2],[9.9, 8.8, 7.7]]
+
+for list in list_of_lists:
+    for i in list:
+        print(i)
+    print()
