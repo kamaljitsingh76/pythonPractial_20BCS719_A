183738a7402e0ae4b3eb9c7971695d0cc055d66e
admin <sainikamaljitsingh@gmail.com>
2022-02-24 11:00:50 +0530

    Python Programs 20BCS-719_A

---
+++ loop1_prog.py
@@ -0,0 +1,8 @@
+num_list = [1, 2, 3]
+alpha_list = ['a', 'b', 'c']
+
+for number in num_list:
+    print(number, end = "  ")
+    for letter in alpha_list:
+        print(letter, end = "")
+    print()
---
+++ prog2_var.py
@@ -0,0 +1,7 @@
+# Variable declaration
+
+x=10
+y="3.763"
+print("Value of x : ", x)
+print(" value of y : ", y)
+print(" The Sum of x and y : ", (x + y)
